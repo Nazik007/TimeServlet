da1e75c60a020ec5b814c8ce6a9597aaf2a4c308
Nazarii Soviak <natesoviak@gmail.com>
2022-12-06 22:48:59 +0200

    Finished the app

--- app/src/test/java/timeservlet/TimeServletTest.java
+++ app/src/test/java/timeservlet/TimeServletTest.java
@@ -3,12 +3,6 @@
  */
 package timeservlet;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+class TimeServletTest {
 
-class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
 }
