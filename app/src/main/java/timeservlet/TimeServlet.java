d266f06a41e58d229e06936c70710a71090c0dcc
Nazarii Soviak <natesoviak@gmail.com>
2022-12-12 12:40:00 +0200

    Added Thymeleaf and Cookies to the project

--- app/src/main/java/timeservlet/TimeServlet.java
+++ app/src/main/java/timeservlet/TimeServlet.java
@@ -17,7 +17,7 @@ import java.time.format.DateTimeFormatter;
 public class TimeServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html; charset=utf-8");
         resp.getWriter().write(parseTime(req));
         resp.getWriter().close();
